c6c977216873b9bf33768e90e4e488e46d397fbc
Danylo <danylo14@yandex.ru>
2023-11-10 17:30:42 +0100

    Ese1 fatto

---
+++ java/eser5/ese1/Punto.java
@@ -0,0 +1,55 @@
+package eser5.ese1;
+//classe punto con metodi e incapsulamento
+public class Punto {
+    private double x;
+    private double y;
+
+    public Punto(double x, double y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    public double getX()
+    {
+        return this.x;
+    }
+    public double getY()
+    {
+        return this.y;
+    }
+    
+    public void setX(double x)
+    {
+        this.x=x;
+    }
+    public void setY(double y)
+    {
+        this.y=y;
+    }
+
+    public String toSring()
+    {
+        return "x:"+this.x+" y:"+this.y;
+    }
+    public boolean equals(Punto p)
+    {
+        if(this.x == p.getX() && this.y == p.getY())
+            return true;
+        else
+            return false;
+    }
+
+    public double Distanza(Punto p)
+    {   //                        ⇊differenza tra le x            ⇊differenza tra le y  
+        return Math.sqrt(Math.pow(this.x-p.getX(), 2)+Math.pow(this.y-p.getY(), 2));//restituisce la distanza tra i due punti
+    }   //      ⇈radice quadrata                    ⇈elevato a 2                   ⇈elevato a 2
+
+    public Punto PuntoMed(Punto p)
+    {   
+        double xx=(this.x+p.getX())/2;//la formula pre prendere il punto medio delle x
+        double yy=(this.y+p.getY())/2;//per le y
+        Punto r=new Punto(xx, yy);
+        return r;
+    }
+
+}
---
+++ java/eser5/ese1/UsoPunto.java
@@ -0,0 +1,36 @@
+package eser5.ese1;
+
+import java.util.Scanner;
+
+public class UsoPunto {
+    public static void print(String str)//mi scoccio scrivere system.out.println
+    {
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        double x, y;
+        Scanner input=new Scanner(System.in);
+        print("inserire la x del primo punto");
+        x=input.nextDouble();
+        print("inserire la y del primo punto");
+        y=input.nextDouble();
+        Punto p1=new Punto(x, y);
+        print("inserire la x del secondo punto");
+        x=input.nextDouble();
+        print("inserire la y del secondo punto");
+        y=input.nextDouble();
+        Punto p2 = new Punto(x, y);
+        if(!p1.equals(p2))// il "!" serve ad invertire il risutlato del metodo equals, perché mi scoccio sistemare il codice...
+        {   //qundi se p1 == p2 il metodo restituira vero e con ! lo nego e diventa falso, e cosi al contrario...
+            print("la distanza tra i due punti è: "+p1.Distanza(p2));
+            print((p1.PuntoMed(p2)).toSring());//in pratica tramite p1 uso il metodo PuntoMed e li passo p2
+            //si come PuntoMed restituisce un ogetto di tipo Punto gli aplico il metodo toString, che restituisce una stringa
+        }else
+        {
+            print("hai inserito 2 punti uguali quindi è inutile calcolare la distanza e il punto medio");
+        }
+        
+        input.close();
+    }
+    
+}
