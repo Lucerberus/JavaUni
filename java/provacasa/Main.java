aa6c1679307d35df8dbc4e84e83f8985ec246910
Danylo <51485381+Lucerberus@users.noreply.github.com>
2023-12-15 12:42:14 +0100

    Create Main.java

---
+++ java/provacasa/Main.java
@@ -0,0 +1,18 @@
+package eser9.ese4;
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{	
+		Scanner input = new Scanner(System.in);
+		Stack l1 = new Stack();
+		Stack l2 = new Stack();
+		
+		
+		
+		
+		
+	}
+}
